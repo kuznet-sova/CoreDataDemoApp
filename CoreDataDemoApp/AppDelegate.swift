a673194031a5dd99e9d3f4b1b72bdac155330260
kuznet.i.sova <kis7425910@gmail.com>
2020-06-30 23:20:43 +0300

    added StorageManager

---
+++ CoreDataDemoApp/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  CoreDataDemoApp
+//
+//  Created by Alexey Efimov on 29.06.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+        
+        return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        StorageManager.storageManager.saveContext()
+    }
+}
+
